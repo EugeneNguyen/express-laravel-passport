662e4c7c39601c7a073ddb575e1a5595ee0543ed
iConcept <iconcept@MacBook-Pro-2.local>
2018-02-06 16:11:36 +0700

    Fix import

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
 require('babel-core/register');
 require('babel-polyfill');
-
-import jwt from 'jsonwebtoken';
-import OauthAccessToken from './OauthAccessToken';
-
-export default async function passport_middleware(request, response, next) {
-  const { headers } = request;
-  if (headers.authorization) {
-    const authorization = headers.authorization;
-    const comp = authorization.split(' ');
-    if (comp.length == 2 && comp[0] == 'Bearer') {
-      const token = comp[1];
-      const { jti } = jwt.decode(token);
-
-      const access_token = await OauthAccessToken.findById(jti);
-      request.user_id = access_token.user_id
-    }
-  }
-  next();
-}
+require('./src/index');
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+import OauthAccessToken from './OauthAccessToken';
+
+export default async function passport_middleware(request, response, next) {
+  const { headers } = request;
+  if (headers.authorization) {
+    const authorization = headers.authorization;
+    const comp = authorization.split(' ');
+    if (comp.length == 2 && comp[0] == 'Bearer') {
+      const token = comp[1];
+      const { jti } = jwt.decode(token);
+
+      const access_token = await OauthAccessToken.findById(jti);
+      request.user_id = access_token.user_id
+    }
+  }
+  next();
+}
